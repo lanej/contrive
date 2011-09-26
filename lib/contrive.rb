e2ff4cc978281284bf5dcb5fad14f9951e0a7cd8
Joshua Lane <jlane@engineyard.com>
2011-09-25 17:27:18 -0700

    resolve through other action

--- lib/contrive.rb
+++ lib/contrive.rb
@@ -15,13 +15,31 @@ module Contrive
       actions << Contrive::Action.new.tap{|ac| ac.instance_eval(&block)}
     end
     def self.resolve(produce)
-      resolution, answer = actions.detect{|action| action.produces.keys.first == produce}.create_resolution(produce)
+      matching_actions = actions.select{|action| action.produces.type == produce}
+      answer = nil
+      matching_actions.detect do |action|
+        action.unresolved_demands.each{|demand| action.resolve_demand(demand, Contrive::Action.resolve(demand))}
+        resolution, answer = action.create_resolution(produce)
+      end
       Contrive::Model.new(answer)
     end
 
-    attr_reader :produces
+    attr_reader :produces, :demands
     def produce(model)
-      @produces = model
+      model = {model => {}} if model.is_a?(Symbol)
+      @produces = Contrive::Model.new(model)
+    end
+    def demand(model)
+      unresolved_demands << model
+    end
+    def unresolved_demands
+      @unresolved_demands ||= []
+    end
+    def resolve_demand(key, value)
+      demands[key] = value
+    end
+    def demands
+      @demands ||= {}
     end
     def create_resolution(model)
       answer = nil
@@ -35,13 +53,11 @@ module Contrive
       resolutions << block
     end
   end
-  class Model
+  class Model < Hash
     attr_reader :type, :attributes
     def initialize(hash)
-      @type, @attributes = hash.keys.first, hash[hash.keys.first]
-    end
-    def [](key)
-      attributes[key]
+      @type = hash.keys.first
+      self.merge!(hash[hash.keys.first])
     end
   end
 end
--- spec/contrive_spec.rb
+++ spec/contrive_spec.rb
@@ -15,4 +15,22 @@ describe Contrive do
     team = Contrive.resolve(:team)
     team[:captain][:name].should eql "bob"
   end
+  it "should build from two actions" do
+    Contrive::Action.build do
+      demand :captain
+      produce :team
+      resolve do |model|
+        {:team => {:captain => demands[:captain]}}
+      end
+    end
+    Contrive::Action.build do
+      produce :captain
+      resolve do |model|
+        {:captain => {:name => "bob"}}
+      end
+    end
+    
+    team = Contrive.resolve(:team)
+    team[:captain][:name].should eql "bob"
+  end
 end
